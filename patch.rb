f0305a4734dfc0902b0d08e3d7a7723531259aa0
Yohsuke Murase <yohsuke.murase@gmail.com>
2016-10-01 17:21:08 +0900

    refactoring

--- patch.rb
+++ patch.rb
@@ -1,21 +1,18 @@
 Simulator  # must eval `Simulator` to ensure Simulator class is loaded
 class Simulator
 
-  def parameter_sets_in_dataframe(where: nil, result_keys: nil)
+  def parameter_sets_in_dataframe(where: nil, with_results: nil)
     pss = parameter_sets.asc(:id)
     pss = pss.where(where) if where
     param_keys = parameter_definitions.map(&:key)
     rows = pss.map do |ps|
       mapped = param_keys.map {|key| ps.v[key] }
-      if result_keys
-        avgs = result_keys.map {|rkey| ps.average_result(rkey)[0] }
-        mapped += avgs
-      end
+      mapped += with_results.map {|r| ps.average_result(r) } if with_results
       mapped
     end
     ids = pss.map(&:id)
 
-    keys = param_keys + result_keys.to_a
+    keys = param_keys + with_results.to_a
 
     Daru::DataFrame.rows( rows, order: keys, index: ids )
   end
@@ -24,12 +21,16 @@ end
 ParameterSet
 class ParameterSet
 
-  def average_result(key)
+  def average_result(key, error: false)
     results = runs.where(status: :finished).only(:result).map {|r| r.result[key] }
     n = results.size
     avg = results.inject(:+) / n
-    r2_sum = results.inject(0.0) {|sum,x| (x-avg)**2}
-    [avg, Math.sqrt(r2_sum/(n-1.0)) ]
+    if error
+      r2_sum = results.inject(0.0) {|sum,x| (x-avg)**2}
+      return [avg, Math.sqrt(r2_sum/(n-1.0)) ]
+    else
+      return avg
+    end
   end
 end
 
